0dab053fe5ceb66988f90f17cee306dea1828353
bigfort1999-tech <bigfort1999@gmail.com>
2026-03-17 22:17:58 -0400

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,4 +1,4 @@
-const stripe = require(‘stripe’)(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
